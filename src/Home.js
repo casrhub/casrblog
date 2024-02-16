a7c2adb67757ea450193850bd55fa23accdc5816
César Silva <casrr@proton.me>
2024-02-16 02:19:52 -0600

    using click events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,15 @@
 const Home = () => {
+    const handleClick = (e) => {
+        console.log("qr pai",e)
+    }
+    const handleClickAgain = (name) => {
+        console.log(name)
+    }
     return (
         <div className="home">
             <h2>Home Page</h2>
+            <button onClick={ handleClick }>Click me</button>
+            <button onClick = { handleClickAgain('casr')}>Click Again</button>
         </div>
     );
 }
